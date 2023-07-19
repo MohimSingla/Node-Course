6b1b7711231f7c89c3325351ee8b4ea1ed830e20
MohimSingla <Mohim.singla@skeps.com>
2023-07-19 15:52:38 +0530

    Module 7: Progress Update: Complete Learnings

--- express/testFile.js
+++ express/testFile.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+app.set('view engine', 'hbs');
+
 
 app.listen(3000, () => { 
     console.log("Server up and running.");
 })
-
-app.use(express.static(__dirname)); // specify file name => it will render 
-
+console.log(__dirname)
+app.use(express.static(__dirname+"/../")); // specify file name => it will render 
+hbs.registerPartials(__dirname+"/partials")
 
 app.get('/weather', (req, res)=> {
     var obj = {
@@ -15,22 +19,32 @@ app.get('/weather', (req, res)=> {
         location: "Random Locantion"
     }
     res.send(obj);
-    console.log(req);
 })
 
 
-// app.get('/', (req, res)=> {
-//     res.sendFile(__dirname+"/index.html");
-//     console.log(req);
-// })
+app.get('/', (req, res)=> {
 
-// app.get('/help', (req, res)=> {
-//     res.sendFile(__dirname+"/help.html");
-//     console.log(req);
-// })
+    res.render('index', {
+        data: "Sending static data from main file",
+        body: "This is a sample paragraph, sending static data from main file and rendering it in run-time."
+    })
+    // // res.sendFile(__dirname+"/index.html");
+    // console.log(req);
+})
+
+app.get('/help', (req, res)=> {
+    res.render('help', {
+        data: "Help Section",
+        body: "Welcome to help section. You'll have to help yourself :)"
+    });
+    // console.log(req);
+})
 
 
-// app.get('/about', (req, res)=> {
-//     res.sendFile(__dirname+"/about.html");
-//     console.log(req);
-// })
+app.get('/about', (req, res)=> {
+    res.render('about', {
+        data: "About Section",
+        body: "Welcome to about section. I'm Dope."
+    });
+    // console.log(req);
+})
